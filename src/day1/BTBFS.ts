418f17ba0971ac52b1815622ee256ddd8e8560a6
Joseph Miocevich <joseph.miocevich@mechanicalrock.io>
2023-06-23 09:38:44 +0800

    -

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,19 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+    const q: (BinaryNode<number> | null)[] = [head];
 
+    while (q.length) {
+        const curr = q.shift() as BinaryNode<number> | null | undefined;
+        console.log("current", curr);
+        console.log("q", q);
+
+        if (!curr) {
+            continue;
+        }
+        if (curr?.value === needle) {
+            return true;
+        }
+        q.push(curr!.right);
+        q.push(curr!.left);
+    }
+    return false;
 }
--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,17 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    walk(head, path);
+    return path;
+}
 
+function walk(curr: BinaryNode<number> | null, path: number[]): number[] {
+    if (!curr) {
+        return path;
+    }
+    path.push(curr.value);
+
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    return path;
 }
